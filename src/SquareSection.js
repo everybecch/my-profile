9ddc78dc3260ab4d9a046383bb17eeb6bd7bd646
everybecch <everton.santos@compasso.com.br>
2023-09-06 15:19:53 -0300

    Feature: Commit Final | #010

---
+++ src/SquareSection.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'; // Importe seu arquivo CSS
+
+const SquareSection = () => {
+  return (
+    <div className="square-section">
+      <div className="square left-square">
+        <p className="square-text">Minha História</p>
+      </div>
+      <div className="square right-square">
+        <p className="square-text">My History</p>
+      </div>
+    </div>
+  );
+};
+
+export default SquareSection;
